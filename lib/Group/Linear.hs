9dbed54eed9ed039823ffaa5273a4231c9d668f0
Melanie Brown <brown.m@pm.me>
2024-04-30 02:04:10 -0400

    ortho :: SL n x -> Maybe (O n x)

--- lib/Group/Linear.hs
+++ lib/Group/Linear.hs
@@ -1,12 +1,14 @@
 module Group.Linear where
 
+import Control.Monad (guard)
+import Data.Functor (($>))
 import Data.Group
 import GHC.TypeNats
 import Linear.Matrix
 import Linear.V
 
 newtype SL n x = SL (V n (V n x))
-  deriving (Semigroup, Monoid, Functor, Foldable, Traversable)
+  deriving (Eq, Semigroup, Monoid, Functor, Foldable, Traversable)
 instance (KnownNat n, Monoid x, Fractional x) => Group (SL n x) where
   invert :: (KnownNat n, Monoid x, Fractional x) => SL n x -> SL n x
   invert (SL mat) = SL (luInvFinite mat)
@@ -15,3 +17,12 @@ sl :: (KnownNat n, Fractional x, Eq x) => V n (V n x) -> Maybe (SL n x)
 sl mat = case abs (luDet mat) of
   1 -> Just (SL mat)
   _ -> Nothing
+
+newtype O n x = O (SL n x)
+  deriving (Eq, Semigroup, Monoid, Functor, Foldable, Traversable)
+instance (KnownNat n, Monoid x, Fractional x) => Group (O n x) where
+  invert :: (KnownNat n, Monoid x, Fractional x) => O n x -> O n x
+  invert (O mat) = O (invert mat)
+
+ortho :: (KnownNat n, Monoid x, Eq x, Fractional x) => SL n x -> Maybe (O n x)
+ortho mat@(SL v) = guard (SL (transpose v) == invert mat) $> O (SL v)
