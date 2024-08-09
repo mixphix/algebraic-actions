dbb604299e893b1358ce6a48e2c2249fbf6be49b
Melanie Brown <brown.m@pm.me>
2024-08-09 18:23:55 -0400

    instance Enum Four

--- lib/Group/F4.hs
+++ lib/Group/F4.hs
@@ -5,9 +5,9 @@ import Data.Group
 data Four
   = Omit
   | Need
-  | Many
   | Free
-  deriving (Eq, Ord, Show)
+  | Many
+  deriving (Eq, Ord, Enum, Read, Show)
 
 newtype F4a = F4a Four deriving (Eq, Ord)
 instance Semigroup F4a where
@@ -15,40 +15,40 @@ instance Semigroup F4a where
   F4a Need <> F4a x = F4a case x of
     Omit -> Need
     Need -> Omit
-    Many -> Free
     Free -> Many
+    Many -> Free
   F4a Many <> F4a x = F4a case x of
     Omit -> Many
     Need -> Free
-    Many -> Omit
     Free -> Need
+    Many -> Omit
   F4a Free <> F4a x = F4a case x of
     Omit -> Free
     Need -> Many
-    Many -> Need
     Free -> Omit
+    Many -> Need
 instance Monoid F4a where mempty = F4a Omit
 instance Group F4a where
   invert = \case
     F4a Omit -> F4a Need
     F4a Need -> F4a Omit
-    F4a Many -> F4a Free
     F4a Free -> F4a Many
+    F4a Many -> F4a Free
 
 newtype F4m = F4m Four deriving (Eq, Ord)
 instance Semigroup F4m where
   F4m Omit <> F4m _ = F4m Omit
   F4m Need <> F4m x = F4m x
-  F4m Many <> F4m x = F4m case x of
-    Omit -> Omit
-    Need -> Many
-    Many -> Free
-    Free -> Need
   F4m Free <> F4m x = F4m case x of
     Omit -> Omit
     Need -> Free
     Many -> Need
     Free -> Many
+  F4m Many <> F4m x = F4m case x of
+    Omit -> Omit
+    Need -> Many
+    Many -> Free
+    Free -> Need
 instance Monoid F4m where mempty = F4m Need
 
 instance Num Four where
@@ -58,13 +58,13 @@ instance Num Four where
   abs = \case
     Omit -> Omit
     Need -> Omit
-    Many -> Need
     Free -> Need
+    Many -> Need
   signum = \case
     Omit -> Omit
     Need -> Need
-    Many -> Need
     Free -> Omit
+    Many -> Need
   fromInteger n = case n `mod` 4 of
     0 -> Omit
     1 -> Need
